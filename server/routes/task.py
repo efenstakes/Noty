f0ea1ca4154978d925b3199788dd46834a8f037e
Efen Stakes <efenstakes101@gmail.com>
2019-10-28 01:27:51 +0300

    add user and task route resources

---
+++ server/routes/task.py
@@ -0,0 +1,66 @@
+from flask_restful import Resource
+
+
+## add
+class TaskAdd(Resource):
+
+    def post(self):
+        responze = { 'saved': False, 'id': None }
+
+        return responze
+
+## update
+class TaskUpdate(Resource):
+
+    def put(self, id):
+        responze = { 'updated': False }
+
+        return responze
+
+## detail
+class TaskDetail(Resource):
+
+    def get(self, id):
+        responze = { 'task': {} }
+
+        return responze
+
+## delete
+class TaskDelete(Resource):
+
+    def delete(self, id):
+        responze = { 'deleted': False }
+
+        return responze
+
+
+## get all
+class TaskAll(Resource):
+
+    def get(self, id):
+        responze = { 'tasks': [] }
+
+        return responze
+
+
+## mark as complete
+class TaskComplete(Resource):
+
+    def post(self, id):
+        responze = { 'updated': False }
+
+        return responze
+
+
+## add or delete participants
+class TaskParticipants(Resource):
+
+    def post(self, id):
+        responze = { 'saved': False, 'id': None }
+
+        return responze
+
+    def delete(self, id):
+        responze = { 'deleted': False }
+
+        return responze
---
+++ server/routes/user.py
@@ -0,0 +1,52 @@
+from flask_restful import Resource
+
+
+
+## add user
+class UserAdd(Resource):
+
+    def post(self):
+        responze = { 'saved': False, 'id': None }
+
+        return responze
+
+
+## get user details
+class UserDetails(Resource):
+    
+    def get(self, id):
+        responze = { 'user': {} }
+
+        return responze
+
+## update user details
+class UserUpdate(Resource):
+    
+    def post(self, id):
+        responze = { 'updated': False }
+
+        return responze
+
+## delete user 
+class UserDelete(Resource):
+    
+    def delete(self, id):
+        responze = { 'deleted': False }
+
+        return responze
+
+## login user
+class UserLogin(Resource):
+    
+    def post(self):
+        responze = { 'user': {}, 'token': None }
+
+        return responze
+
+## logout user
+class UserLogout(Resource):
+    
+    def post(self):
+        responze = { 'logged_out': False }
+
+        return responze
