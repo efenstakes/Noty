7c31760cd9271042c458cc5393399a0ae723bdc6
Efen Stakes <efenstakes101@gmail.com>
2019-10-28 14:08:50 +0300

    add methods to check if name or email is used in user model

--- server/models/user.py
+++ server/models/user.py
@@ -1,5 +1,6 @@
 import mongoengine
 import datetime
+from werkzeug.security import check_password_hash
 
 
 class User(mongoengine.Document):
@@ -14,3 +15,17 @@ class User(mongoengine.Document):
     def __repr__(self):
         return 'Im {0} and my email is {1}'.format(self.name, self.email)
 
+
+    ''' check if a password matches with this objects hashed password '''
+    def is_hash_match(self, password):
+        return check_password_hash(self.password, password)
+
+    ''' check if name is used '''
+    def name_used(self):
+        matches = self.objects(name=self.name)
+        return len(matches) > 0
+    
+    ''' check if email is used '''
+    def email_used(self):
+        matches = self.objects(email=self.email)
+        return len(matches) > 0
