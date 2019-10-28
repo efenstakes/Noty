ade3c084099475aa0a6bb04b22d1fc82fc8ae27c
Efen Stakes <efenstakes101@gmail.com>
2019-10-28 12:48:52 +0300

    add user model

---
+++ server/models/user.py
@@ -0,0 +1,16 @@
+import mongoengine
+import datetime
+
+
+class User(mongoengine.Document):
+    name = mongoengine.StringField( required=True, max_length=50 )
+    email = mongoengine.EmailField( required=True, max_length=50 )
+    password = mongoengine.StringField( required=True )
+    joined_on = mongoengine.DateTimeField( default=datetime.datetime.utcnow )
+
+    ## define collection name
+    meta = { 'collection': 'users' }
+
+    def __repr__(self):
+        return 'Im {0} and my email is {1}'.format(self.name, self.email)
+
