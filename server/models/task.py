0c28dcef2a9a6f226bd7635875da3d07d14099aa
Efen Stakes <efenstakes101@gmail.com>
2019-10-28 12:49:06 +0300

    add task model

---
+++ server/models/task.py
@@ -0,0 +1,21 @@
+import mongoengine
+import datetime
+
+
+class Task(mongoengine.Document):
+    title = mongoengine.StringField( max_length=100 )
+    body = mongoengine.StringField()
+    starts_on = mongoengine.DateTimeField( default=None )
+    ends_on = mongoengine.DateTimeField( default=None )
+    is_complete = mongoengine.BooleanField( default=False )
+    participants = mongoengine.ListField( mongoengine.StringField() )
+    # user = mongoengine.ReferenceField('User', reverse_delete_rule=mongoengine.CASCADE)
+    added_on = mongoengine.DateTimeField( default = datetime.datetime.utcnow )
+
+    ## define document name
+    meta = { 'collection': 'tasks' }
+
+    def __repr__(self):
+        return 'Task >> {0} starts on >> {1} and ends on >> {2}'.format(self.title, self.starts_on, self.ends_on)
+        
+
