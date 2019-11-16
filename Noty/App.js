e43df545c0367cf526abfd2cbaf6c01d425ce3f9
Efen Stakes <efenstakes101@gmail.com>
2019-11-16 22:22:14 +0300

    add app activities

--- Noty/App.js
+++ Noty/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
-import {
-  StyleSheet, View, Text
-} from 'react-native'
+import { View, Text } from 'react-native'
 
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
+
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+ 
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+
+
+
+// app colors
+let primary_color = '#00A1DE'
+let secondary_color = '#2cdeea'
+let bottom_navigation_bar_color = '#003F72'
+let active_tab_color = '#00A1DE'
+let inactive_tab_color = '#2cdeea'
 
 
 // splash screen
@@ -17,6 +33,18 @@ const SplashScreenActivity = () => {
 }// const SplashScreenActivity = () => { .. }
 
 
+// Tutorial screen
+const TutorialActivity = () => {
+
+  return(
+    <View>
+      <Text> Tutorial Screen </Text>
+    </View>
+  )
+
+}// const TutorialScreenActivity = () => { .. }
+
+
 // login screen
 const LoginActivity = () => {
 
@@ -27,6 +55,7 @@ const LoginActivity = () => {
   )
 
 }// const LoginActivity = () => { .. }
+
 
 // register screen
 const RegisterActivity = () => {
@@ -39,16 +68,24 @@ const RegisterActivity = () => {
 
 }// const RegisterActivity = () => { .. }
 
-// home screen
-const HomeActivity = () => {
 
-  return(
-    <View>
-      <Text> home Screen </Text>
-    </View>
-  )
+// home screen
+class HomeActivity extends React.Component {
+  static navigationOptions = {
+    // header: null, headerLeft: null,
+    title: 'Explore Our Services'
+  }
+  
+  render() {
+    return(
+      <View>
+        <Text> home Screen </Text>
+      </View>
+    )
+  }
 
 }// const HomeActivity = () => { .. }
+
 
 // user profile screen
 const UserProfileActivity = () => {
@@ -60,6 +97,7 @@ const UserProfileActivity = () => {
   )
 
 }// const UserProfileActivity = () => { .. }
+
 
 // settings screen
 const SettingsActivity = () => {
@@ -73,21 +111,189 @@ const SettingsActivity = () => {
 }// const SettingsActivity = () => { .. }
 
 
-const App = () => {
-  return (
-    <View>
- 
-      <Text> Noty </Text>
-      <LoginActivity/>
 
+/**
+ * route stacks
+ */
+const HomeStack = createStackNavigator({
+  Home: { 
+    screen: HomeActivity,
 
-    </View>
-  )
-}
+    navigationOptions: {
+      title: 'Home'
+    }
 
-const styles = StyleSheet.create({
-  
+  },
+})
+const UserProfileStack = createStackNavigator({
+  UserProfile: { 
+    screen: UserProfileActivity,
+
+    navigationOptions: {
+      title: 'Profile'
+    }
+
+  },
+})
+const SettingsStack = createStackNavigator({
+  Settings: { 
+    screen: SettingsActivity,
+
+    navigationOptions: {
+      title: 'Settings'
+    }
+
+  },
 })
 
+
+// create app routes
+const BottomTabNav = createMaterialBottomTabNavigator({
+
+  Home: {
+    screen: HomeStack,
+    
+    navigationOptions:{
+      header: null,  
+      headerLeft: null, 
+      tabBarLabel:'Home',  
+      tabBarIcon:({tintColor})=>(  
+        <View>
+          <Icon name="info-circle" color={tintColor} size={25}/>  
+        </View>
+      )  
+    } 
+
+  },
+
+  UserProfile: {
+    screen: UserProfileStack,
+    
+    navigationOptions:{
+      header: null,  
+      headerLeft: null, 
+      tabBarLabel:'Profile',  
+      tabBarIcon:({tintColor})=>(  
+        <View>
+          <Icon name="info-circle" color={tintColor} size={25}/>  
+        </View>
+      )  
+    } 
+
+  },
+
+  Settings: {
+    screen: SettingsStack,
+    
+    navigationOptions:{
+      header: null,  
+      headerLeft: null, 
+      tabBarLabel:'Settings',  
+      tabBarIcon:({tintColor})=>(  
+        <View>
+          <Icon name="info-circle" color={tintColor} size={25}/>  
+        </View>
+      )  
+    } 
+
+  }
+
+
+},
+{
+
+  shifting: false,
+  initialRouteName: 'Home',
+  activeColor: primary_color, // active_tab_color, // '#f0edf6',
+  inactiveColor: '#7C7F7D', // '#3e2465',
+  barStyle: { 
+    backgroundColor: 'white', 
+    justifyContent: 'space-between', 
+    alignItems: 'center' 
+  },
+
+  headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false,
+  }
+
+})
+
+
+
+
+// create the app wide navigator
+const AppNav = createSwitchNavigator({
+
+    Tutorial: {
+      screen: TutorialActivity,  navigationOptions: {}
+    },
+
+    Splash: {
+      screen: SplashScreenActivity, navigationOptions: {}
+    },
+
+    Login: {
+      screen: LoginActivity, 
+      navigationOptions: { title: 'Login' }
+    },
+
+    // Register: RegisterStack,
+    Register: {
+      screen: RegisterActivity, 
+      navigationOptions: { 
+        title: 'Create An Account Now' 
+      }
+    },
+
+    AppHome: {
+      screen: BottomTabNav
+    }
+
+  },
+  { 
+    initialRouteName: 'AppHome', // 'Splash',
+    defaultNavigationOptions: {
+
+      // headerStyle: {
+      //   backgroundColor: '#00A1DE',
+      // },
+      // headerTintColor: '#00A1DE', // '#fff',
+      // headerTitleStyle: {
+      //   fontWeight: 'bold',
+      // }
+
+    } 
+  }  
+)
+
+// use above config to create app component with the set routing configurations
+const AppContainer = createAppContainer(AppNav)
+
+
+// app theme
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: primary_color,
+    accent: secondary_color,
+  },
+  fonts: {
+    regular: 'noa',
+    medium: 'noa',
+    light: 'verdana',
+    thin: 'verdana',
+  },
+}
+
+const App = () => {
+  return (
+    <PaperProvider theme={theme}>
+      <AppContainer/>
+    </PaperProvider>
+  )
+}// const App = () => { .. }
 
 export default App
